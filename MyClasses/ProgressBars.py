9365a1caf78bcc30fde318656d151f279ecf1e8d
Elden <eldenlxh-sm22@student.tarc.edu.my>
2024-12-20 20:23:43 +0800

    Big update to gui, ToDo, ProgressBars

--- MyClasses/ProgressBars.py
+++ MyClasses/ProgressBars.py
@@ -1,38 +1,48 @@
 import tkinter as tk
 
 class ProgressBars:
-  def __init__(self, parent, length, thickness, wallpaper_colour, track_colour, fill_colour):
+  def __init__(self, parent, side, length, thickness, wallpaper_colour, track_colour, fill_colour, external_function):
     self.__parent = parent
+    self.__side = side
     self.__length = length
     self.__thickness = thickness
     self.__wallpaper_colour = wallpaper_colour
     self.__track_colour = track_colour
     self.__fill_colour = fill_colour
+    self.__external_function = external_function
     
-    # Creating a square background
-    self.__canvas = tk.Canvas(self.__parent, width=self.__length, height=self.__length, bg=self.__wallpaper_colour)
-    self.__canvas.pack()
+    # Creating a square background                                              -Elden
+    self.__canvas = tk.Canvas(self.__parent, width=self.__length, height=self.__length, bg=self.__wallpaper_colour, highlightbackground=self.__wallpaper_colour)
+    self.__canvas.pack(side=self.__side)
+
+    #bind the progress bar to a mouseclick                                      -Elden
+    self.__canvas.bind("<Button-1>", self.on_click)
         
-    # Draw the background circle
+    # Draw the background circle                                                -Elden
     self.__progress_track = self.__canvas.create_oval(self.__thickness, self.__thickness, self.__length-self.__thickness, self.__length-self.__thickness, 
                                               outline=self.__track_colour, width=self.__thickness)
         
-    # Draw the foreground arc (progress bar)
+    # Draw the foreground arc (progress bar)                                    -Elden
     self.__progress_fill = self.__canvas.create_arc(self.__thickness, self.__thickness, self.__length-self.__thickness, self.__length-self.__thickness, 
                                                start=90, extent=0, outline=self.__fill_colour, width=self.__thickness, style="arc")
         
     # Create a text item in the center to display the progress value
     self.__text = self.__canvas.create_text(self.__length/2, self.__length/2, text="0%", font=("Arial", 14, "bold"))
 
+  #Update the progress                                                          -Elden
   def setProgress(self, progress):
-    #exception handling
-    # Limit value between 0 and 100
-    # value = max(0, min(value, 100))
     self.__canvas.itemconfig(self.__text, text=f"{progress}%")
 
-    progress = progress/100 * 360
+    progress = int(progress)/100 * 360
     self.__canvas.itemconfig(self.__progress_fill, extent=progress)
+  
+  #Execute external functions                                                   -Elden
+  def on_click(self, event):
+    self.__external_function()
+
+  def on_click_with_event(self, event):
+    self.__external_function(event)
 
   # use
-  # progress_bar = ProgressBar(root, 200, 20, "white", "black", "green")
+  # progress_bar = ProgressBar(root, 200, 20, "white", "black", "green", function())
   # progress_bar.setprogress(10)
